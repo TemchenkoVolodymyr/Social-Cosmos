0d4f8007320ca58115c59a252e16e535a2abcd71
TemchenkoVolodymyr <vovatemcenko4@gmail.com>
2023-08-21 20:39:21 +0200

    choose bg for Layout

--- src/Router/Layout.js
+++ src/Router/Layout.js
@@ -4,25 +4,28 @@ import {Outlet} from "react-router";
 import {NavLink} from "react-router-dom";
 import {useSelector} from "react-redux";
 import Auth from "../Pages/AuthPage/Auth";
+import bg from '../assets/bg3.png'
 
 const Layout = () => {
 
   const isAuth = useSelector((state) => state.isAuth);
   const currentUser = useSelector((state) => state.user)
-  console.log(currentUser)
-  if(!isAuth) {
+
+  if (!isAuth) {
     return <Auth></Auth>
   }
   return (
     <>
       <section>
-        <header>HEADER</header>
-        <div className={style.container}>
-          <nav>
-          </nav>
-          <main>
-            <Outlet></Outlet>
-          </main>
+        <div className={style.wrapper} style={{backgroundImage:`url(${bg})`}}>
+          <header>HEADER</header>
+          <div className={style.container}>
+            <nav>
+            </nav>
+            <main>
+              <Outlet></Outlet>
+            </main>
+          </div>
         </div>
       </section>
     </>
